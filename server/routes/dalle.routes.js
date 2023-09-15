c38839bbf0eb3abbcf17d8c8b8e58161c8d55685
Abhay Chaudhary <126079996+abhaychaudhary0@users.noreply.github.com>
2023-09-15 23:36:16 +0530

    Update dalle.routes.js

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import * as dotenv from 'dotenv';
+// import * as dotenv from 'dotenv';
 import { Configuration, OpenAIApi } from 'openai';
 
-dotenv.config();
+// dotenv.config();
 
 const router = express.Router();
 const config = new Configuration({
